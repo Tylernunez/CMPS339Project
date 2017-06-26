3c4972b3b7b2d0868afe4ca4ff33793e695bb9a5
James Ostarly <james.ostarly@selu.edu>
2017-06-25 20:39:58 -0500

    created column class, began work on dbms create table function

---
+++ project1/project1/Column.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Column
+    {
+        private string name;
+        private Type type;
+   
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public Type getType()
+        {
+            return this.type;
+        }
+
+        public void setType(Type type)
+        {
+            this.type = type;
+        }
+    }
+
+ 
+}
---
+++ project1/project1/dbms.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace project1
+{
+    class dbms
+    {
+        public void createTable(String tableName, List<Column> columns, String colName)
+        {
+            DataTable dt = new DataTable(tableName);
+            dt.Clear();
+
+            foreach (var i in columns)
+            {
+                Type temp = i.getType();
+                //dt.Columns.Add(i.getName(), typeof();
+            }
+        }
+    }
+}
